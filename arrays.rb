bd04908c51fbaae9bf09eac72ab087d1b4c90376
Mokoth <onyach.okoth1@outlook.com>
2022-09-20 12:08:56 +0300

    for-loop ~ same file

--- arrays.rb
+++ arrays.rb
@@ -12,5 +12,19 @@ while stuff.length != 10
   stuff.push(next_one)
   puts "There are #{stuff.length} items now."
 end
+puts "There we go: #{stuff}"
+
+
+# With for-loop
+=begin
+for value in mystuff do
+  if stuff.length != 10
+    next_one = value
+    puts "Adding: #{value}"
+    stuff.push(value)
+    puts "There are #{stuff.length} items now."
+  end
+end
 
 puts "There we go: #{stuff}"
+=end
